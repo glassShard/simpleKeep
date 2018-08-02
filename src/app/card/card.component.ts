214007be4f401676b46da25962bf968f7edb7268
glassShard <info@uvegszilank.hu>
2018-08-02 12:24:46 +0200

    add card comp

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,26 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {CardModel} from '../core/card-model';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input() card: CardModel;
+  @Output() viewClicked = new EventEmitter();
+  @Output() deleteClicked = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onViewClick() {
+    this.viewClicked.emit({id: this.card.id});
+  }
+
+  onDeleteClick() {
+    this.deleteClicked.emit({id: this.card.id});
+  }
+}
